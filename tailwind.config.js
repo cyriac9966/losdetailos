eb2ab048be8a5e176b7f987479d4ec72faaeadf2
Cyriac-Ernest <103205781+cyriac9966@users.noreply.github.com>
2025-10-20 16:22:23 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        brand: "rgb(253,188,70)"
+      }
+    }
   },
+  darkMode: "class",
   plugins: []
 };
